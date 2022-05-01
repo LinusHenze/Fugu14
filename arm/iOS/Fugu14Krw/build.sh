bf955595427f3dc277b71e31c8978b7491cbc317
dan <dlevi309@users.noreply.github.com>
2022-04-30 20:03:58 -0700

    fix missing arm64e slice from FuguKrw plugin, although it's incomplete ATM because the code that generates the trust cache for that probably needs to be adjusted to include both archs

--- arm/iOS/Fugu14Krw/build.sh
+++ arm/iOS/Fugu14Krw/build.sh
@@ -8,9 +8,32 @@ do
     swiftBuild+=(-Xswiftc "$arg")
 done
 
-echo Building Fugu14Krw
+echo Building Fugu14Krw64
 echo ${swiftBuild[*]}
 ${swiftBuild[*]}
 
+mv .build/release/libFugu14Krw.dylib libFugu14Krw-64.dylib
+rm -rf .build
+
+swiftcArgs=(-sdk "`xcrun --sdk iphoneos --show-sdk-path`" -target arm64e-apple-ios14.0 -O -framework IOKit)
+
+swiftBuild=(swift build -c release -Xcc "-DIOS_BUILD" -Xcc -target -Xcc arm64e-apple-ios14.0 -Xcc -Wno-incompatible-sysroot)
+for arg in ${swiftcArgs[*]}
+do
+    swiftBuild+=(-Xswiftc "$arg")
+done
+
+echo Building Fugu14Krw64e
+echo ${swiftBuild[*]}
+${swiftBuild[*]}
+
+mv .build/release/libFugu14Krw.dylib libFugu14Krw-64e.dylib
+
+echo gonna make them kiss
+
+lipo -create libFugu14Krw-64.dylib libFugu14Krw-64e.dylib --output libFugu14Krw.dylib
+
+rm libFugu14Krw-64.dylib libFugu14Krw-64e.dylib
+
 echo Signing Fugu14Krw
-codesign -s - .build/release/libFugu14Krw.dylib
+codesign -s - libFugu14Krw.dylib
